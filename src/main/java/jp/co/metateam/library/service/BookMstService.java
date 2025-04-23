7926fcf10cdb2a144b350ca2fd17c9aeaa643828
taiga-yamada-mt <taiga.yamada@metateam.co.jp>
2025-04-23 20:12:54 +0900

    書籍登録画面

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -42,8 +42,79 @@ public class BookMstService {
 
         return bookMstDtoList;
     }
-    
+    public void save(BookMstDto bookMstDto){
+        try{
+            BookMst book = new BookMst(); //newはクラスを作る　BookMstクラスから
+
+            book.setTitle(bookMstDto.getTitle()); //HTMLの処理をここに
+            book.setIsbn(bookMstDto.getIsbn());
+
+            //データベースへの保存
+            this.bookMstRepository.save(book);
+        } catch(Exception e) {
+            throw e;
+        }
+    }
+
+    //バリデーション処理
+    //errTitleListは("""""""")と何個も書ける、Stringは("")一個
+    public boolean isValidTitle(String title, Model model) {
+        if (StringUtils.isEmpty(title)) {
+            model.addAttribute("errTitle", "書籍名を入力してください");
+            return true;
+        } 
+        //書籍名未入力
+
+        if (title.length() > 255){
+            model.addAttribute("errTitle", "書籍名は255文字以内で入力してください");
+                return true;
+        } 
+        //書籍名256文字以上
+        return false;
+            
+    }  
+    public boolean isValidIsbn(String isbn, Model model) {
+        if (StringUtils.isEmpty(isbn)) {
+            model.addAttribute("errIsbn", "ISBNを入力してください");
+            return true;
+        } 
+        //ISBN未入力
+
+        if (isbn.length() != 13){
+            model.addAttribute("errIsbn", "ISBNは13桁で入力してください");
+                
+
+        } 
+        //ISBN13桁以外
+
+        if (!isbn.matches("\\d+")) {
+            model.addAttribute("errIsbn", "ISBNは半角数字で入力してください");
+
+            
+            return true;
+        }
+        //ISBN半角数字以外
+        return false;
+        
+    }
+        //is○○はboolean
+
+    //重複チェック用のメソッド
+    public boolean selectByIsbn(String isbn , Model model){
+        List<BookMst> book = this.bookMstRepository.selectByIsbn(isbn);
+
+        if(book.size() !=0){
+        model.addAttribute("errIsbn","このISBNは既に登録されています");
+        return true;
+        }
+    return false;
+    }
 }
+
+ 
+
+
+
 
 
 
